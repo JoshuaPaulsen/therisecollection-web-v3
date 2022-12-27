4e2fc1880ad71aa5dc77e1b0ad76482cc306048b
Joshua Paulsen <joshuapaulsen@Joshuas-MacBook-Pro.local>
2022-12-26 21:15:01 -0500

    snap to scroll implemented, updated favicon and tab-text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div
+        style={{
+          backgroundColor: 'red'
+        }}
+        className="section"
+      ></div>
+
+      <div
+        className="section"
+        style={{
+            backgroundColor: 'orange'
+
+        }}
+      ></div>
+      <div
+        className="section"
+        style={{
+            backgroundColor: 'yellow'
+
+        }}
+      ></div>
+      <div
+        className="section"
+        style={{
+            backgroundColor: 'green'
+        }}
+      ></div>
+      <div
+        className="section"
+        style={{
+            backgroundColor: 'purple'
+
+        }}
+      ></div>
+      <div
+        className="section"
+        style={{
+            backgroundColor: 'pink'
+        }}
+      ></div>
+      <div
+        className="section"
+        style={{
+            backgroundColor: 'red'
+        }}
+      ></div>
     </div>
   );
 }
-
-export default App;
