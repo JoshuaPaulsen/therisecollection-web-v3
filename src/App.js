be1c2c07d8045ce8db87e594dd276291f97f9d8f
Joshua Paulsen <joshuapaulsen@Joshuas-MacBook-Pro.local>
2022-12-27 21:55:02 -0500

    started work on header, implemented css for PHONE and LARGER DEVICES

--- src/App.js
+++ src/App.js
@@ -2,51 +2,52 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="container">
+      <div className="container">
+        <div className="flex-box-header header">
+          <h2>THE RISE COLLECTION</h2>
+          <button id="menu" className="button">MENU</button>
+          </div>
       <div
         style={{
           backgroundColor: 'red'
         }}
         className="section"
-      ></div>
-
+      >
+      </div>
       <div
         className="section"
         style={{
-            backgroundColor: 'orange'
-
+          backgroundColor: 'orange'
         }}
       ></div>
       <div
         className="section"
         style={{
-            backgroundColor: 'yellow'
-
+          backgroundColor: 'yellow'
         }}
       ></div>
       <div
         className="section"
         style={{
-            backgroundColor: 'green'
+          backgroundColor: 'green'
         }}
       ></div>
       <div
         className="section"
         style={{
-            backgroundColor: 'purple'
-
+          backgroundColor: 'purple'
         }}
       ></div>
       <div
         className="section"
         style={{
-            backgroundColor: 'pink'
+          backgroundColor: 'pink'
         }}
       ></div>
       <div
         className="section"
         style={{
-            backgroundColor: 'red'
+          backgroundColor: 'red'
         }}
       ></div>
     </div>
